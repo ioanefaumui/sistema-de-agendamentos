b51b7b7fee2d6f0c856675c24d5f0e722d14daef
ioanefaumui <johnfaumui@gmail.com>
2025-02-15 02:21:25 -0300

    feat(user): add initial user files

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const origin = process.env.CORS_ORIGIN || 'http://localhost:3000';
+  app.enableCors({
+    origin,
+  });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 void bootstrap();
