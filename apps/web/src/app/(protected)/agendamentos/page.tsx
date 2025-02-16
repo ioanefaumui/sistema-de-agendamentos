365f053394bba24872e3f3ae0294ceb9ffe4dd00
ioanefaumui <johnfaumui@gmail.com>
2025-02-16 09:37:58 -0300

    feat(appointments): add appointments initial page structure

---
+++ apps/web/src/app/(protected)/agendamentos/page.tsx
@@ -0,0 +1,9 @@
+export default function Servicos() {
+  return (
+    <main className="w-full">
+      <div className="w-[min(100%-2rem,78.5rem)] mx-auto">
+        <h1 className="mt-4">Página de agendamentos</h1>
+      </div>
+    </main>
+  );
+}
