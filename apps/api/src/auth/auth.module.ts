3acaba130414f907cd63a5f5d7601201edbca45f
ioanefaumui <johnfaumui@gmail.com>
2025-02-18 22:24:58 -0300

    fix: remove comments

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -1,23 +1,22 @@
-// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport'; // Import PassportModule
+import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { UsersService } from 'src/users/users.service';
-import { JwtStrategy } from './jwt.strategy'; // Import your JwtStrategy
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     UsersModule,
-    PassportModule.register({ defaultStrategy: 'jwt' }), // Register Passport with default strategy
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '7d' },
     }),
   ],
-  providers: [UsersService, AuthService, JwtStrategy], // Provide JwtStrategy
+  providers: [UsersService, AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
