76be217fac9506d3c95a7258651ce52c69e9a980
ioanefaumui <johnfaumui@gmail.com>
2025-02-18 21:37:50 -0300

    feat: add lib and utils functions

--- apps/web/src/features/auth/utils/login-form-schema.ts
+++ apps/web/src/features/auth/utils/login-form-schema.ts
@@ -3,6 +3,6 @@
 import { z } from "zod";
 
 export const loginFormSchema = z.object({
-  email: z.string().email(),
+  email: z.string().email({ message: "Email inválido" }),
   password: z.string().min(1, { message: "Insira sua senha" }),
 });
--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -1,1 +1,29 @@
+import { cookies } from "next/headers";
 export const api = process.env.API_URL || "http://localhost:3001";
+
+export async function serverApiClient(
+  input: RequestInfo,
+  init?: RequestInit
+): Promise<Response> {
+  const cookieStore = await cookies();
+
+  const token = cookieStore.get("token")?.value;
+
+  const headers = new Headers(init?.headers || {});
+
+  if (token) {
+    headers.set("Authorization", `Bearer ${token}`);
+  }
+
+  if (!headers.has("Content-Type")) {
+    headers.set("Content-Type", "application/json");
+  }
+
+  const newInit: RequestInit = {
+    ...init,
+    headers,
+    cache: "no-store",
+  };
+
+  return fetch(input, newInit);
+}
